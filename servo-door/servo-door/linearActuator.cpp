d055869e78e544a917ae8831db4fd0fa36a48086
cam <cameron.ryan.pears@gmail.com>
2021-05-18 10:04:03 +1200

    Added servo-door version of software

---
+++ servo-door/servo-door/linearActuator.cpp
@@ -0,0 +1,88 @@
+#include "linearActuator.h"
+#include "util.h"
+#include "Config.h"
+
+LinearActuator::LinearActuator(int pwmPin, int sensePin, int forwardPin, int backPin) {
+  _pwmPin = pwmPin;
+  _sensePin = sensePin;
+  _forwardPin = forwardPin;
+  _backPin = backPin;
+}
+
+void LinearActuator::setup() {
+  pinMode(_sensePin, INPUT);
+  pinMode(_forwardPin, OUTPUT);
+  pinMode(_backPin, OUTPUT);
+  pinMode(_pwmPin, OUTPUT);
+  digitalWrite(_forwardPin, LOW);
+  digitalWrite(_backPin, LOW);
+  digitalWrite(_pwmPin, LOW);
+  TCCR1B = TCCR1B & B11111000 | B00000001;    // set timer 1 divisor to     1 for PWM frequency of 31372.55 Hz
+}
+
+void LinearActuator::init() {
+  Serial.print("Running LinearActuator init...  ");
+  Serial.println("Done.");
+}
+
+void LinearActuator::back() {
+  Serial.println("Moving Linear actuator back");
+  digitalWrite(_forwardPin, LOW);
+  digitalWrite(_backPin, LOW);
+  delay(100);
+  digitalWrite(_pwmPin, LOW);
+  digitalWrite(_backPin, HIGH);
+  delay(100);
+  rampPWMOn();
+  waitForNoCurrent();
+  rampPWMOff();
+  digitalWrite(_forwardPin, LOW);
+  digitalWrite(_backPin, LOW);
+}
+
+
+void LinearActuator::forward() {
+  Serial.println("Moving Linear actuator forward");
+  digitalWrite(_forwardPin, LOW);
+  digitalWrite(_backPin, LOW);
+  delay(100);
+  digitalWrite(_pwmPin, LOW);
+  digitalWrite(_forwardPin, HIGH);
+  delay(100);
+  rampPWMOn();
+  waitForNoCurrent();
+  rampPWMOff();
+  digitalWrite(_forwardPin, LOW);
+  digitalWrite(_backPin, LOW);
+}
+
+void LinearActuator::waitForNoCurrent() {
+  int count = 0;
+  while (count < 10) {
+    if (analogRead(_sensePin) == 0) {
+      count++;
+    } else {
+      count = 0;
+    }
+  }
+}
+
+void LinearActuator::rampPWMOn() {
+  int i = 0;
+  while (i < 255) {
+    analogWrite(_pwmPin, i);
+    delay(2);
+    i++;
+  }
+  digitalWrite(_pwmPin, HIGH);
+}
+
+void LinearActuator::rampPWMOff() {
+  int i = 255;
+  while (i > 0) {
+    analogWrite(_pwmPin, i);
+    delay(2);
+    i--;
+  }
+  digitalWrite(_pwmPin, LOW);
+}
---
+++ servo-door/servo-door/servo-door.ino
@@ -0,0 +1,177 @@
+// Main trap has one PIR sensor in the middle. When the PIR detects movement
+// in the given time window the trap will trigger, hopefully causing the pest
+// into one of the holding cages. After 10 minutes the trap will open up again
+// ready to catch the next pest.
+
+
+#include "rtc.h"
+#include "util.h"
+#include "Config.h"
+#include "linearActuator.h"
+#include <Servo.h>
+
+LinearActuator linearActuator(LA_PWM, LA_SENSE, LA_FORWARD, LA_BACK);
+RTC rtc; //TODO Rename to time Window
+
+unsigned long lastMovementTime = 0;
+unsigned long baitTriggerTime = 0;
+Servo trapServo;
+Servo doorServo;
+
+enum State {
+  WAITING_FOR_ACTIVE_WINDOW,
+  WAITING_FOR_MAIN_TRIGGER,
+  WAITING_FOR_RESET
+};
+
+State state = WAITING_FOR_MAIN_TRIGGER;
+
+void setup() {
+  //======= SETUP IO PINS ================
+  linearActuator.setup();
+  rtc.setup();
+  pinMode(PIR, INPUT);
+  pinMode(BEAM, INPUT_PULLUP);
+  pinMode(SERVO_PIN, OUTPUT);
+  pinMode(SERVO_DOOR_PIN, OUTPUT);
+  pinMode(ENABLE_6V_PIN, OUTPUT);
+  pinMode(LED_STATUS_PIN, OUTPUT);
+  pinMode(BAIT_PIN, OUTPUT);
+  digitalWrite(BAIT_PIN, LOW);
+
+
+  //============ INIT SYSTEMS ===============
+  Serial.begin(57600);
+  Serial.println("Start init systems");
+  rtc.init();
+  linearActuator.init();
+  initServo();
+  resetTrap();
+  triggerBait();
+  rtc.isInActiveWindow(true); // Print out time status
+  Serial.println(F("Finished init"));
+  
+  blinkStatus(STATUS_STARTING, false);
+  setState(WAITING_FOR_MAIN_TRIGGER);
+  Serial.println(F("Waiting for PIR to trigger"));
+}
+
+void setState(State newState) {
+  if (newState == state) {
+    return;
+  }
+  state = newState;
+  switch (state) {
+    case WAITING_FOR_ACTIVE_WINDOW:
+      Serial.println(F("Switching state to: Waiting for active window"));
+      break;
+    case WAITING_FOR_MAIN_TRIGGER:
+      Serial.println(F("Switching state to: Waiting for main trigger"));
+      break;
+    case WAITING_FOR_RESET:
+      Serial.println(F("Switching state to: Waiting for trap reset"));
+      break;
+    default:
+      Serial.println(F("Switching to unknown state"));
+      break;
+  }
+}
+
+void loop() {
+  switch (state) {
+    case WAITING_FOR_ACTIVE_WINDOW:
+      if (rtc.isInActiveWindow(false)) {
+        rtc.isInActiveWindow(true);
+        Serial.println("Active window started. Waiting for main trigger.");
+        state = WAITING_FOR_MAIN_TRIGGER;
+      }
+      break;
+
+    case WAITING_FOR_MAIN_TRIGGER:
+      if (!rtc.isInActiveWindow(false)) {
+        rtc.isInActiveWindow(true);
+        Serial.println("No longer in active window. Waiting for active window.");
+        state = WAITING_FOR_ACTIVE_WINDOW;
+        break;
+      }
+      //Waiting for the PIR to be triggered
+      if (digitalRead(PIR) == LOW) {
+        Serial.println("Main PIR triggered. Closing blinds");
+        lastMovementTime = millis();
+
+        moveServo(trapServo, SERVO_OPEN_ANGLE, 1000);
+        moveServo(trapServo, SERVO_CLOSED_ANGLE, 1000);
+        state = WAITING_FOR_RESET;
+        Serial.println("Waiting for animal to move into cage");
+      }
+      // Check if food should be dispensed
+      if (millis() - baitTriggerTime > BAIT_DELAY) {
+        triggerBait();
+      }
+      break;
+
+    case WAITING_FOR_RESET:
+      if (digitalRead(PIR) == LOW) {
+        Serial.println("Movement detected, reseting wait time.");
+        lastMovementTime = millis();
+      }
+      if (digitalRead(BEAM) == LOW) {
+        closeDoor();
+        blinkStatus(STATUS_CODE_CAUGHT_PEST, true);
+      }
+      if (millis() - lastMovementTime > RESET_WAIT_TIME) {
+        Serial.println("Waited long enough for the pest to move into a cage. Resetting trap.");
+        resetTrap();
+        state = WAITING_FOR_MAIN_TRIGGER;
+      }
+      break;
+  }
+}
+
+//===========SERVOS===============
+void initServo() {
+  Serial.print("Running Servo init...  ");
+  trapServo.attach(SERVO_PIN);
+  doorServo.attach(SERVO_DOOR_PIN);
+  moveServo(trapServo, SERVO_CLOSED_ANGLE, 1000);
+  moveServo(doorServo, SERVO_DOOR_OPEN_ANGLE, 2000);
+  Serial.println("Done.");
+}
+
+void moveServo(Servo s, int angle, int mill) {
+  s.write(angle);
+  digitalWrite(ENABLE_6V_PIN, HIGH);
+  delay(mill);
+  digitalWrite(ENABLE_6V_PIN, LOW);
+}
+
+void closeDoor() {
+  moveServo(doorServo, SERVO_DOOR_CLOSED_ANGLE, 4000);
+  moveServo(doorServo, SERVO_DOOR_LOCKED_ANGLE, 2000);
+}
+
+//===================BAIT==============
+void initBait() {
+  Serial.print("Running Bait init...  ");
+  triggerBait();
+  Serial.println("Done");
+}
+
+void triggerBait() {
+  Serial.println("running bait");
+  digitalWrite(BAIT_PIN, HIGH);
+  delay(BAIT_DURATION);
+  digitalWrite(BAIT_PIN, LOW);
+  baitTriggerTime = millis();
+  delay(10000); // Wait 10 seconds so if the bait movement triggered the PIR it has time to turn off.
+}
+
+
+//===================================
+void resetTrap() {
+  linearActuator.back();
+  linearActuator.forward();
+  moveServo(trapServo, SERVO_OPEN_ANGLE, 1000);
+  moveServo(trapServo, SERVO_CLOSED_ANGLE, 1000);
+  linearActuator.back();
+}
